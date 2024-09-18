be69db2e32495abedc773fbadc80687cd5214b70
victorsz08 <joaovictorsal777@gmail.com>
2024-09-18 12:28:37 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,16 @@ import "dotenv/config";
 const port = process.env.PORT || 3333;
 const app = express();
 
-app.use(cors({
-    optionsSuccessStatus: 200,
-    origin: "*",
-    methods: "POST, GET, PUT, DELETE"    
-}))
 
 
 app.get("/", (request: Request, response: Response) => {
     return response.status(200).send("Notetools API");
 });
 
+app.use(cors())
+
 routes(app);
+
 
 app.listen(port, () => {
     console.log(`server running in port: ${port}`);
