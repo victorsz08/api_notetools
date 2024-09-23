e93df40fad339c3328a22bdc960a7612b9c804ee
victorsz08 <joaovictorsal777@gmail.com>
2024-09-23 08:50:24 -0300

    Ajuste CORS

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,16 @@ import "dotenv/config";
 const port = process.env.PORT || 3333;
 const app = express();
 
-
+app.use(cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    methods: "POST, DELETE, PUT, GET",
+}))
 
 app.get("/", (request: Request, response: Response) => {
     return response.status(200).send("Notetools API");
 });
 
-app.use(cors({
-    origin: ["https://notetools.online", "https://www.notetools.online"]
-}))
 
 routes(app);
 
